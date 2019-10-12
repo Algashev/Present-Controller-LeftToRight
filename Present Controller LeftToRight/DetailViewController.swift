68df1b5c885baa0d4cdac0cf8c60d187831644f3
Alexander Algashev <algashev@mail.ru>
2019-10-12 15:45:46 +0300

    Completed Project

---
+++ Present Controller LeftToRight/DetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailViewController.swift
+//  Present Controller LeftToRight
+//
+//  Created by Александр Алгашев on 12/10/2019.
+//  Copyright © 2019 Александр Алгашев. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBAction func dismissController(_ sender: Any) {
+        self.dismissDetail()
+    }
+}
--- Present Controller LeftToRight/ViewController.swift
+++ Present Controller LeftToRight/ViewController.swift
@@ -9,12 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func presentController(_ sender: Any) {
+        let detailViewController = DetailViewController(nibName: nil, bundle: nil)
+        detailViewController.modalPresentationStyle = .fullScreen
+        self.presentDetail(detailViewController)
     }
-
-
 }
 
