49abd1a916b138e0d76ac7ad14a0053a651ab44a
Alexander Algashev <algashev@mail.ru>
2019-10-12 15:10:44 +0300

    Initial Commit

---
+++ Present Controller LeftToRight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Present Controller LeftToRight
+//
+//  Created by Александр Алгашев on 12/10/2019.
+//  Copyright © 2019 Александр Алгашев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
